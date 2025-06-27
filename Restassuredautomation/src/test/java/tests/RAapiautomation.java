f73586395d4ed53ba8b1ce667713dfb85b2d8e28
Bhagyaraj1218 <113296866+Bhagyaraj1218@users.noreply.github.com>
2025-06-27 08:29:44 +0530

    Modified api project

---
+++ Restassuredautomation/src/test/java/tests/RAapiautomation.java
@@ -0,0 +1,60 @@
+package tests;
+	
+	import io.restassured.RestAssured;
+	import io.restassured.response.Response;
+	import io.restassured.http.ContentType;
+	import static org.junit.Assert.*;
+
+import org.testng.annotations.Test;
+	
+
+	public class RAapiautomation {
+
+	    @Test(priority=1)
+	    public void testPostApiResponsePositive() {
+	        String apiURL = "https://reqres.in/api/login";
+
+	        String requestBody = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\"}";
+	        System.out.println(requestBody);
+
+	        Response response = RestAssured
+	                .given()
+	                .header("x-api-key","reqres-free-v1")               
+	                .contentType(ContentType.JSON)
+	                .body(requestBody)
+	                .when()
+	                .post(apiURL);
+
+	        int statusCode = response.getStatusCode();
+	        String responseBody = response.getBody().asString();
+
+	        assertEquals(200, statusCode);
+	       System.out.println(responseBody);
+	    }
+	    
+	    
+	    @Test(priority=2)
+	    
+	    public void testPostApiResponseNegative() {
+	        String apiURL = "https://reqres.in/api/login";
+
+	        String requestBody = "{ \"email\": \"test@gmail.com\", \"password\": \"test\"}";
+	        System.out.println(requestBody);
+
+	        Response response = RestAssured
+	                .given()
+	                .header("x-api-key","reqres-free-v1")               
+	                .contentType(ContentType.JSON)
+	                .body(requestBody)
+	                .when()
+	                .post(apiURL);
+
+	        int statusCode = response.getStatusCode();
+	        String responseBody = response.getBody().asString();
+
+	        assertEquals(400, statusCode);
+	       System.out.println(responseBody);
+	    }
+	}
+
+
