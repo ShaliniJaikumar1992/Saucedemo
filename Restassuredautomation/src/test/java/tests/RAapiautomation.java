bd5e26f771157a8e08789259763e8f01843290d6
Bhagyaraj1218 <113296866+Bhagyaraj1218@users.noreply.github.com>
2025-06-27 14:49:22 +0530

    updated test execution file

--- Restassuredautomation/src/test/java/tests/RAapiautomation.java
+++ Restassuredautomation/src/test/java/tests/RAapiautomation.java
@@ -1,60 +1,68 @@
 package tests;
-	
-	import io.restassured.RestAssured;
-	import io.restassured.response.Response;
-	import io.restassured.http.ContentType;
-	import static org.junit.Assert.*;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.http.ContentType;
+import static org.junit.Assert.*;
 
 import org.testng.annotations.Test;
-	
 
-	public class RAapiautomation {
 
-	    @Test(priority=1)
-	    public void testPostApiResponsePositive() {
-	        String apiURL = "https://reqres.in/api/login";
+public class RAapiautomation {
 
-	        String requestBody = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\"}";
-	        System.out.println(requestBody);
+	@Test(priority=1)
+	public void testPostApiResponsePositive() {
+		String apiURL = "https://reqres.in/api/login";
+		System.out.println("Login in valid credentials");
+		String requestBody = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\"}";
+		System.out.println("Request payload");
+		System.out.println(requestBody);
 
-	        Response response = RestAssured
-	                .given()
-	                .header("x-api-key","reqres-free-v1")               
-	                .contentType(ContentType.JSON)
-	                .body(requestBody)
-	                .when()
-	                .post(apiURL);
+		Response response = RestAssured
+				.given()
+				.header("x-api-key","reqres-free-v1")               
+				.contentType(ContentType.JSON)
+				.body(requestBody)
+				.when()
+				.post(apiURL);
 
-	        int statusCode = response.getStatusCode();
-	        String responseBody = response.getBody().asString();
+		int statusCode = response.getStatusCode();
+		String responseBody = response.getBody().asString();
 
-	        assertEquals(200, statusCode);
-	       System.out.println(responseBody);
-	    }
-	    
-	    
-	    @Test(priority=2)
-	    
-	    public void testPostApiResponseNegative() {
-	        String apiURL = "https://reqres.in/api/login";
-
-	        String requestBody = "{ \"email\": \"test@gmail.com\", \"password\": \"test\"}";
-	        System.out.println(requestBody);
-
-	        Response response = RestAssured
-	                .given()
-	                .header("x-api-key","reqres-free-v1")               
-	                .contentType(ContentType.JSON)
-	                .body(requestBody)
-	                .when()
-	                .post(apiURL);
-
-	        int statusCode = response.getStatusCode();
-	        String responseBody = response.getBody().asString();
-
-	        assertEquals(400, statusCode);
-	       System.out.println(responseBody);
-	    }
+		assertEquals(200, statusCode);
+		System.out.println("Verify the status code is coming as 200:"+statusCode);
+		System.out.println("Response payload");
+		System.out.println(responseBody);
 	}
+
+
+	@Test(priority=2)
+
+	public void testPostApiResponseNegative() {
+		String apiURL = "https://reqres.in/api/login";
+		System.out.println("Login in Invalid credentials");
+		String requestBody = "{ \"email\": \"test@gmail.com\", \"password\": \"test\"}";
+		System.out.println("Request payload");
+		System.out.println(requestBody);
+
+		Response response = RestAssured
+				.given()
+				.header("x-api-key","reqres-free-v1")               
+				.contentType(ContentType.JSON)
+				.body(requestBody)
+				.when()
+				.post(apiURL);
+
+		int statusCode = response.getStatusCode();
+		String responseBody = response.getBody().asString();
+
+		assertEquals(400, statusCode);
+
+		System.out.println("Verify the status code is coming as 400:"+statusCode);
+		System.out.println("Response payload");
+
+		System.out.println(responseBody);
+	}
+}
 
 
